03ff07da5ee91c57ccba7f99ee964ac5ca0aa92b
Ondrej Sika <ondrej@ondrejsika.com>
2020-09-30 21:56:23 +0200

    Add Youtube video player with stream of first meetup

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
+import React from "react";
+import ReactPlayer from "react-player";
+
 const Page = () => (
   <div style={{ fontFamily: "sans-serif" }}>
     <center>
       <h1>Vercel Czech Community</h1>
+      <ReactPlayer url="https://www.youtube.com/watch?v=W8iR82vHi1c" />
     </center>
   </div>
 );
